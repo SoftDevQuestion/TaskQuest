98974812770289e34985720ecfbaf48f2ebb9407
Mahsa Rezaei Dinani <mahsadinani80@gmail.com>
2026-01-01 23:32:02 +0330

    حل مشکلات پیش آمده + تغییرات فونت

--- SideBar.Master.designer.cs
+++ SideBar.Master.designer.cs
@@ -87,6 +87,15 @@ namespace TaskQuest
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl liProjects;
 
         /// <summary>
+        /// projectSection control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl projectSection;
+
+        /// <summary>
         /// upSideBarProjects control.
         /// </summary>
         /// <remarks>
