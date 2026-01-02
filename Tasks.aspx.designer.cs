83ec980e24f65c38229807e8d583328403007027
Mahsa Rezaei Dinani <mahsadinani80@gmail.com>
2026-01-02 04:15:09 +0330

    رفع خطاهای اولیه ی صفحه ی وظایف

--- Tasks.aspx.designer.cs
+++ Tasks.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace TaskQuest
 
     public partial class Tasks
     {
+
+        /// <summary>
+        /// rptProjects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptProjects;
+
+        /// <summary>
+        /// lblNoProjects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNoProjects;
     }
 }
