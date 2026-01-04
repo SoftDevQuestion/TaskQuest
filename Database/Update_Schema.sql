07c43f14a40835a996216c4987828c3a21e3876b
Mahsa Rezaei Dinani <mahsadinani80@gmail.com>
2026-01-04 14:33:58 +0330

    تثبیت نهایی اسکریپت‌های اصلاح دیتابیس و مدیریت حذف آبشاری

--- Database/Update_Schema.sql
+++ Database/Update_Schema.sql
@@ -27,40 +27,47 @@ BEGIN
     BEGIN TRY
         BEGIN TRANSACTION;
 
-        -- 1. Identify Teams Created by User
+        -- 0. Get User ID
+        DECLARE @UserId INT;
+        SELECT @UserId = UserID FROM Users WHERE Username = @Username;
+
+        -- 1. Delete UserProfile (Direct Child of Users)
+        IF @UserId IS NOT NULL
+        BEGIN
+            DELETE FROM UserProfile WHERE UserId = @UserId;
+        END
+
+        -- 2. Identify Teams Created by User
         DECLARE @CreatedTeams TABLE (TeamId INT);
         INSERT INTO @CreatedTeams SELECT TeamId FROM Team WHERE CreatorUsername = @Username;
 
-        -- 2. Identify Projects related to these teams
+        -- 3. Identify Projects related to these teams
         DECLARE @TeamProjects TABLE (ProjectId INT);
         INSERT INTO @TeamProjects 
         SELECT ProjectId FROM Projects WHERE TeamAccessId IN (SELECT TeamId FROM @CreatedTeams);
 
-        -- 3. Delete TASKS associated with these projects
+        -- 4. Delete TASKS associated with these projects
         DELETE FROM Tasks WHERE ProjectId IN (SELECT ProjectId FROM @TeamProjects);
 
-        -- 4. Delete ProjectTeams for these projects
+        -- 5. Delete ProjectTeams for these projects
         DELETE FROM ProjectTeams WHERE ProjectID IN (SELECT ProjectId FROM @TeamProjects);
 
-        -- 5. Delete these Projects
+        -- 6. Delete these Projects
         DELETE FROM Projects WHERE ProjectId IN (SELECT ProjectId FROM @TeamProjects);
 
-        -- 6. Delete TeamMembers for Created Teams
+        -- 7. Delete TeamMembers for Created Teams
         DELETE FROM TeamMembers WHERE TeamId IN (SELECT TeamId FROM @CreatedTeams);
 
-        -- 7. Delete ProjectTeams for Created Teams
+        -- 8. Delete ProjectTeams for Created Teams
         DELETE FROM ProjectTeams WHERE TeamID IN (SELECT TeamId FROM @CreatedTeams);
 
-        -- 8. Delete Created Teams
+        -- 9. Delete Created Teams
         DELETE FROM Team WHERE TeamId IN (SELECT TeamId FROM @CreatedTeams);
 
-        -- 9. Clean up User membership in OTHER teams
+        -- 10. Clean up User membership in OTHER teams
         DELETE FROM TeamMembers WHERE Username = @Username;
 
-        -- 10. Delete Projects created by user (orphaned)
-        DECLARE @UserId INT;
-        SELECT @UserId = UserID FROM Users WHERE Username = @Username;
-
+        -- 11. Delete Projects created by user (orphaned)
         IF @UserId IS NOT NULL
         BEGIN
             DECLARE @UserProjects TABLE (ProjectId INT);
@@ -69,12 +76,9 @@ BEGIN
             DELETE FROM Tasks WHERE ProjectId IN (SELECT ProjectId FROM @UserProjects);
             DELETE FROM ProjectTeams WHERE ProjectID IN (SELECT ProjectId FROM @UserProjects);
             DELETE FROM Projects WHERE ProjectId IN (SELECT ProjectId FROM @UserProjects);
-
-            -- NEW: Delete UserProfile
-            DELETE FROM UserProfile WHERE UserId = @UserId;
         END
 
-        -- 11. Delete User
+        -- 12. Delete User
         DELETE FROM Users WHERE Username = @Username;
 
         COMMIT TRANSACTION;
